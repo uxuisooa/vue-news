50aa2dfcb1903a78763658f3c1da7abc5793da2b
uxuisooa <sooa0513@naver.com>
2020-03-22 21:16:22 +0900

    ToolBar의 라우터 링크 스타일링

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="header">
     <router-link to="/news">News</router-link>
     <router-link to="/ask">Ask</router-link>
     <router-link to="/jobs">Jobs</router-link>
@@ -10,5 +10,17 @@
 export default {};
 </script>
 
-<style>
+<style scoped>
+.header {
+  color: #fff;
+  background: #42b883;
+  display: flex;
+  padding: 8px;
+}
+.header router-link-exact-active {
+  color: #35495e;
+}
+.header a {
+  color: #fff;
+}
 </style>
